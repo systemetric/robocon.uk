58e2888702670175b838d7c92acd4935d079d93f
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-11-30 17:17:11 +0000

    Make carousel image scale. Add some content

--- src/components/HomepageCarouselItem.svelte
+++ src/components/HomepageCarouselItem.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
-    import * as Carousel from "@/components/ui/carousel/index.ts"
-    let {src = "", alt=""} = $props();
+    import * as Carousel from "@/components/ui/carousel";
+    import { Button } from "@/components/ui/button";
+    import type { Snippet } from "svelte";
+    interface Props {
+        src: string;
+        alt?: string;
+        title: string;
+        action?: string;
+        children?: Snippet;
+    }
+    let { src, alt, title, action, children }: Props = $props();
+    if (alt === undefined) {
+        console.warn("No image alt text supplied!");
+    }
 </script>
+
 <Carousel.Item>
-    <img src={src} alt={alt}/>
+    <div class="w-full h-[80vh] overflow-hidden justify-center">
+        <img {src} {alt} class="w-full h-full object-cover" />
+    </div>
+    <div class="sm:w-4/5 w-96">
+        <h1>{title}</h1>
+        {@render children?.()}
+        <Button>
+            {action}
+        </Button>
+    </div>
 </Carousel.Item>
