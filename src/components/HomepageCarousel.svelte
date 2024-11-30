8a0f3ebd71004ec5d1d2fc21f54db4d31d2f1334
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-11-30 17:17:11 +0000

    Make carousel image scale. Add some content

--- src/components/HomepageCarousel.svelte
+++ src/components/HomepageCarousel.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import * as Carousel from "@/components/ui/carousel/index.ts";
+    import HomepageCarouselItem from "@/components/HomepageCarouselItem.svelte";
 </script>
 
-<Carousel.Root class="w-20 h-20 left-32">
+<Carousel.Root class="w-full h-fit">
     <Carousel.Content class="bg-red-400">
-        <Carousel.Item>A</Carousel.Item>
+        <HomepageCarouselItem src="/images/IMG_7311.JPG" />
         <Carousel.Item>B</Carousel.Item>
         <Carousel.Item>C</Carousel.Item>
     </Carousel.Content>
-    <Carousel.Previous class="top-auto bottom-1 -left-0"/>
-    <Carousel.Next class="top-auto bottom-1"/>
+    <Carousel.Previous class="top-auto bottom-1 left-10"/>
+    <Carousel.Next class="top-auto bottom-1 right-auto left-20"/>
 </Carousel.Root>
