b13a6899ef60082d791a3ce9c4e3a99a5ee20d05
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-11-30 12:31:30 +0000

    Create homepage carousel from shadcn

---
+++ src/components/HomepageCarousel.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import * as Carousel from "@/components/ui/carousel/index.ts";
+</script>
+
+<Carousel.Root class="w-20 h-20 left-32">
+    <Carousel.Content class="bg-red-400">
+        <Carousel.Item>A</Carousel.Item>
+        <Carousel.Item>B</Carousel.Item>
+        <Carousel.Item>C</Carousel.Item>
+    </Carousel.Content>
+    <Carousel.Previous class="top-auto bottom-1 -left-0"/>
+    <Carousel.Next class="top-auto bottom-1"/>
+</Carousel.Root>
---
+++ src/components/HomepageCarouselItem.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import * as Carousel from "@/components/ui/carousel/index.ts"
+    let {src = "", alt=""} = $props();
+</script>
+<Carousel.Item>
+    <img src={src} alt={alt}/>
+</Carousel.Item>
