b08d600fb958c6789cb2be61346140e3c421a661
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-22 16:57:02 +0000

    Add Rubik Mono

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -55,7 +55,9 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: [...fontFamily.sans],
+				display:["Rubik Mono One", ...fontFamily.sans],
+
 			}
 		}
 	},
