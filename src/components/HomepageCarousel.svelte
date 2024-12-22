a1b1af50785feee91f6960150d84aea3c879a80d
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-22 16:57:02 +0000

    Add new card location, colours

--- src/components/HomepageCarousel.svelte
+++ src/components/HomepageCarousel.svelte
@@ -9,6 +9,15 @@
         <Carousel.Item>B</Carousel.Item>
         <Carousel.Item>C</Carousel.Item>
     </Carousel.Content>
-    <Carousel.Previous class="top-auto bottom-1 left-10"/>
-    <Carousel.Next class="top-auto bottom-1 right-auto left-20"/>
+    <div
+        class="w-4/5 sm:w-72 bg-background absolute bottom-12 left-8 px-4 py-2"
+    >
+        <h1 class="font-display text-lg">Title</h1>
+        <div class="text-xs">body text</div>
+        <Button class="h-6">Action</Button>
+    </div>
+    <Carousel.Previous class="top-auto -bottom-2 scale-75 left-10" />
+    <Carousel.Next
+        class="top-auto -bottom-2 scale-75 right-auto left-[4.5rem]"
+    />
 </Carousel.Root>
