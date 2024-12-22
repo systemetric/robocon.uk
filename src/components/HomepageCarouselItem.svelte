a3d1a8872dfd6dc14bb18e6b9b6fcc244bfa45ff
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2024-12-22 16:57:02 +0000

    Remove props from homepage item, tidy a bit.

--- src/components/HomepageCarouselItem.svelte
+++ src/components/HomepageCarouselItem.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
     import * as Carousel from "@/components/ui/carousel";
-    import { Button } from "@/components/ui/button";
     import type { Snippet } from "svelte";
     interface Props {
         src: string;
         alt?: string;
-        title: string;
-        action?: string;
-        children?: Snippet;
     }
-    let { src, alt, title, action, children }: Props = $props();
+    let { src, alt }: Props = $props();
     if (alt === undefined) {
         console.warn("No image alt text supplied!");
     }
@@ -18,12 +14,5 @@
 <Carousel.Item>
     <div class="w-full h-[80vh] overflow-hidden justify-center">
         <img {src} {alt} class="w-full h-full object-cover" />
-    </div>
-    <div class="sm:w-4/5 w-96">
-        <h1>{title}</h1>
-        {@render children?.()}
-        <Button>
-            {action}
-        </Button>
     </div>
 </Carousel.Item>
